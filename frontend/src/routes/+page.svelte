a76870bb794ba50c5fc20c27390ce16b1cccc62e
rimuspp <19101das@gmail.com>
2023-02-24 10:11:44 -0500

    Added Tailwind typography and forms

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-5xl text-slate-300">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
